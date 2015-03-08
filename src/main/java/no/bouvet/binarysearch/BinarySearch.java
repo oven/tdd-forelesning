0d3a3365c99b382973e5d46be10474ead1721154
Ove Gram Nipen <ove@nipen.no>
2015-03-08 12:17:54 +0100

    Solution to BinarySearch example

--- src/main/java/no/bouvet/binarysearch/BinarySearch.java
+++ src/main/java/no/bouvet/binarysearch/BinarySearch.java
@@ -3,7 +3,21 @@ package no.bouvet.binarysearch;
 public class BinarySearch {
 
     public static int find(int needle, int[] haystack) {
+
+        int low = 0;
+        int high = haystack.length;
+
+        for (int i = 0; i < log2(haystack.length) + 1; i++) {
+            int middle = (high + low) / 2;
+            if (haystack[middle] == needle) return middle;
+            if (haystack[middle] > needle) high = middle;
+            if (haystack[middle] < needle) low = middle;
+        }
         return -1;
+    }
+
+    private static int log2(int n) {
+        return (int) (Math.log(n) / Math.log(2));
     }
 
 }
--- src/test/java/no/bouvet/binarysearch/BinarySearchTest.java
+++ src/test/java/no/bouvet/binarysearch/BinarySearchTest.java
@@ -2,11 +2,53 @@ package no.bouvet.binarysearch;
 
 import org.junit.Test;
 
+import static no.bouvet.binarysearch.BinarySearch.find;
+import static org.junit.Assert.assertEquals;
+
 public class BinarySearchTest {
 
     @Test
-    public void nothing() throws Exception {
-        
+    public void can_find_middle_element() throws Exception {
+        int[] haystack = {2, 4, 6, 8, 10};
+        int index = find(6, haystack);
+        assertEquals(2, index);
+    }
+
+    @Test
+    public void can_find_lower_quarter_element() throws Exception {
+        int[] haystack = {2, 4, 6, 8, 10};
+        int index = find(4, haystack);
+        assertEquals(1, index);
+    }
+
+    @Test
+    public void can_find_upper_quarter_element() throws Exception {
+        int[] haystack = {2, 4, 6, 8, 10};
+        int index = find(8, haystack);
+        assertEquals(3, index);
+    }
+
+    @Test
+    public void can_find_first_element() throws Exception {
+        int[] haystack = {2, 4, 6, 8, 10};
+        int index = find(2, haystack);
+        assertEquals(0, index);
+    }
+
+    @Test
+    public void can_find_last_element() throws Exception {
+        int[] haystack = {2, 4, 6, 8, 10};
+        int index = find(10, haystack);
+        assertEquals(4, index);
+    }
+
+    @Test
+    public void can_find_any_element() throws Exception {
+        int[] haystack = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        for (int i = 0; i < 11; i++) {
+            int index = find(i, haystack);
+            assertEquals(i, index);
+        }
     }
 
 }
