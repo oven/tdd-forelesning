8d4f00029976a44de26cd7bd52904fab33eca7ef
Ove Gram Nipen <ove@nipen.no>
2015-03-08 20:43:39 +0100

    Add scaffolding for BinarySearch example

---
+++ src/main/java/no/bouvet/binarysearch/BinarySearch.java
@@ -0,0 +1,9 @@
+package no.bouvet.binarysearch;
+
+public class BinarySearch {
+
+    public static int find(int needle, int[] haystack) {
+        return -1;
+    }
+
+}
---
+++ src/test/java/no/bouvet/binarysearch/BinarySearchTest.java
@@ -0,0 +1,12 @@
+package no.bouvet.binarysearch;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+    @Test
+    public void nothing() throws Exception {
+        
+    }
+
+}
